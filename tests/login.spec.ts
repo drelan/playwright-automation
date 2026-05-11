ea2afc36d3e899286be1c5dd19d0d742b8b48849
Deepak Relan <derelan@gmail.com>
2026-05-10 23:26:20 -0700

    FIx the browser issue causing failure.

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,22 +1,18 @@
-import { test, expect, Browser, Page, Locator } from '@playwright/test';
-import {webkit, chromium, firefox} from 'playwright';
+import { test, expect, Page } from '@playwright/test';
 
-test('login test', async() =>{
-    const myBrowser:Browser = await chromium.launch({headless: true, channel: 'chrome'});
-    const myPage:Page = await myBrowser.newPage();
-    await myPage.goto("https://demo.applitools.com");
-    const emailId:Locator =  myPage.locator('#username');
-    const password:Locator = myPage.locator('#password');
-    const loginButton:Locator = myPage.locator('#log-in');
+test('login test', async ({ page }) => {
+    await page.goto("https://demo.applitools.com");
+    const emailId = page.locator('#username');
+    const password = page.locator('#password');
+    const loginButton = page.locator('#log-in');
 
-    await emailId.fill("testing")
+    await emailId.fill("testing");
     await password.fill("testing@123!");
     await loginButton.click();
     
-    const title = await myPage.title();
-    console.log("home page title: ", title)
+    const title = await page.title();
+    console.log("home page title: ", title);
 
-    await myPage.screenshot({path: 'homepage.png'});
+    await page.screenshot({ path: 'homepage.png' });
     expect(title).toEqual('ACME demo app');
-    myBrowser.close();
 });
